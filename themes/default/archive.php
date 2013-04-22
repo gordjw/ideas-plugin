96ec9ae5a613b90b7088284b7b592913e7d4b57d
Gordon W <gordjw@gmail.com>
2013-04-22 10:50:49 +0000

    Order by number of votes

--- themes/default/archive.php
+++ themes/default/archive.php
@@ -17,6 +17,19 @@ if ( !defined('ABSPATH')) exit;
  * @link           http://codex.wordpress.org/Theme_Development#Archive_.28archive.php.29
  * @since          available since Release 1.0
  */
+$args = array(
+	"post_type"			=> "idea",
+	"meta_key"			=> "votes",
+	"meta_query"		=> array(
+		array(
+			"key"		=> "votes",
+		),
+	),
+	"orderby"			=> "meta_value_num",
+	"order"				=> "DESC",
+);
+
+query_posts( $args );
 
 get_header(); ?>
 
